27d35598166fe85a53e2268ebc5e8188f47f8b3b
Ashley910 <104787757+Ashley910@users.noreply.github.com>
2026-07-03 20:02:36 -0700

    Create orders_cleaning_queries.sql

---
+++ orders_cleaning_queries.sql
@@ -0,0 +1,11 @@
+------------------------------------------------------------
+-- STEP 1: Explore the Sales tables
+------------------------------------------------------------
+
+-- Explore SalesOrderHeader
+SELECT TOP 50 *
+FROM Sales.SalesOrderHeader;
+
+-- Explore SalesOrderDetail
+SELECT TOP 50 *
+FROM Sales.SalesOrderDetail;
